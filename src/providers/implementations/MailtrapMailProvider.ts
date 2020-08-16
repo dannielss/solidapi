d383cdc3bcc52e5701aeb43fbb2c28374e2b6b60
Daniel <daniel_sousa.p@outlook.com>
2020-08-15 22:08:28 -0300

    feat(/): add implementations

---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,34 @@
+import { IMailProvider, IMessage } from "../IMailProvider";
+import nodemailer from 'nodemailer';
+import Mail from "nodemailer/lib/mailer";
+
+export class MailtrapMailProvider implements IMailProvider {
+  private transporter: Mail;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: 'smpt.mailtrap.io',
+      port: 2525,
+      auth: {
+        user: '71ffe0d316ecd4',
+        pass: 'b2596b1a8948fc'
+      }
+    })
+  }
+
+  async sendMail(message: IMessage): Promise<void> {
+    await this.transporter.sendMail({
+      to: {
+        name: message.to.name,
+        address: message.to.email,
+      },
+      from: {
+        name: message.from.name,
+        address: message.from.email,
+      },
+      subject: message.subject,
+      html: message.body
+
+    })
+  }
+}
--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,10 +1,14 @@
 import { IUsersRepository } from "../../repositories/IUsersRepository"
 import { ICreateUserRequestDTO } from "./CreateUserDTO"
 import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
 
 export class CreateUserUseCase {
 
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(
+    private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider
+  ) {}
 
   async execute(data: ICreateUserRequestDTO) {
     const userAlreadyExists = await this.usersRepository.findByEmail(data.email);
@@ -16,5 +20,18 @@ export class CreateUserUseCase {
     const user = new User(data);
 
     await this.usersRepository.save(user);
+
+    this.mailProvider.sendMail({
+      to: {
+        name: data.name,
+        address: data.email
+      },
+      from: {
+        name: 'Equipe do Meu App',
+        address: 'equipe@meuapp.com'
+      },
+      subject: 'Seja bem vindo à plataforma',
+      body: '<p>Você já pode fazer login em nossa plataforma.</p>'
+    })
   }
 }
